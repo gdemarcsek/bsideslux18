fe426d8dfb4e4b186102a99c9fe733fc10c37648
Gyorgy Demarcsek <dgy.jr92@gmail.com>
2018-10-16 02:41:00 +0200

    gui in image

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,14 @@ Vagrant.configure("2") do |config|
   config.vm.box = "bsideslux18/apparmor"
   config.vm.network "forwarded_port", guest: 8080, host: 8080, host_ip: "127.0.0.1"
   config.vm.network "forwarded_port", guest: 5601, host: 5601, host_ip: "127.0.0.1"
+  config.vm.provider :virtualbox do |v, override|
+        v.customize ["modifyvm", :id, "--vram", "256"]
+        v.customize ["setextradata", "global", "GUI/MaxGuestResolution", "any"]
+        v.customize ["setextradata", :id, "CustomVideoMode1", "1024x768x32"]
+        v.customize ["modifyvm", :id, "--ioapic", "on"]
+        v.customize ["modifyvm", :id, "--rtcuseutc", "on"]
+        v.customize ["modifyvm", :id, "--accelerate3d", "on"]
+        v.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+  end
   config.vm.synced_folder ".", "/vagrant", disabled: true
 end
---
+++ image/scripts/desktop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+SSH_USER=${SSH_USERNAME:-vagrant}
+
+echo "==> Checking version of Ubuntu"
+. /etc/lsb-release
+
+    echo "==> Installing ubuntu-gnome-desktop"
+    apt-get install --yes --force-yes -o Dpkg::Options::="--force-confold" gnome-shell ubuntu-gnome-desktop
+
+    USERNAME=${SSH_USER}
+    LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
+    GDM_CUSTOM_CONFIG=/etc/gdm/custom.conf
+
+    #mkdir -p $(dirname ${GDM_CUSTOM_CONFIG})
+    #echo "[daemon]" >> $GDM_CUSTOM_CONFIG
+    #echo "# Enabling automatic login" >> $GDM_CUSTOM_CONFIG
+    #echo "AutomaticLoginEnable=True" >> $GDM_CUSTOM_CONFIG
+    #echo "AutomaticLoginEnable=${USERNAME}" >> $GDM_CUSTOM_CONFIG
+
+    #echo "==> Configuring lightdm autologin"
+    #echo "[SeatDefaults]" >> $LIGHTDM_CONFIG
+    #echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG
+
+if [ -d /etc/xdg/autostart/ ]; then
+    echo "==> Disabling screen blanking"
+NODPMS_CONFIG=/etc/xdg/autostart/nodpms.desktop
+    echo "[Desktop Entry]" >> $NODPMS_CONFIG
+    echo "Type=Application" >> $NODPMS_CONFIG
+    echo "Exec=xset -dpms s off s noblank s 0 0 s noexpose" >> $NODPMS_CONFIG
+    echo "Hidden=false" >> $NODPMS_CONFIG
+    echo "NoDisplay=false" >> $NODPMS_CONFIG
+    echo "X-GNOME-Autostart-enabled=true" >> $NODPMS_CONFIG
+    echo "Name[en_US]=nodpms" >> $NODPMS_CONFIG
+    echo "Name=nodpms" >> $NODPMS_CONFIG
+    echo "Comment[en_US]=" >> $NODPMS_CONFIG
+    echo "Comment=" >> $NODPMS_CONFIG
+fi
+
+echo "Setting system locale to US"
+locale-gen
+localectl set-locale LANG="en_US.UTF-8"
+
